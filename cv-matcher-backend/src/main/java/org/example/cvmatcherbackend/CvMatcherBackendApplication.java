0cf4c8501fa1adbe40c8e5f9bfe0e779d0ff0cbe
amine-ghazoui <ghazouimohamedamine@gmail.com>
2025-06-19 14:22:32 +0100

    Initial commit: structure du projet avec dossiers backend, frontend, NLP

---
+++ cv-matcher-backend/src/main/java/org/example/cvmatcherbackend/CvMatcherBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.cvmatcherbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvMatcherBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvMatcherBackendApplication.class, args);
+    }
+
+}
---
+++ cv-matcher-backend/src/test/java/org/example/cvmatcherbackend/CvMatcherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cvmatcherbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvMatcherBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
